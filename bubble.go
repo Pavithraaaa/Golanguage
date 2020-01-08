133efd8866e1a53924b47cc0de1c441001b96770
Pavithraaaa <pavithragowda811@gmail.com>
2020-01-08 17:40:21 +0530

    Update bubble.go

--- bubble.go
+++ bubble.go
@@ -4,8 +4,8 @@ import "fmt"
 
 func main() {
 	a := []int{1, 5, 3, 4, 2}
-	for i := 0;i < 5;i++ {       n
-		for j:=i+1; j < 5; j++ { n-1
+	for i := 0;i < 5;i++ {       
+		for j:=i+1; j < 5; j++ { 
 			if a[i] > a[j] {
 				temp := a[i]
 				a[i] = a[j]
